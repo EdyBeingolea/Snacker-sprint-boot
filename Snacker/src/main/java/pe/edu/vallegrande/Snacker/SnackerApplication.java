324355b7d302c98709ce568622038bb30d362a92
EdyBeingolea <edy.beingolea@vallegrande.edu.pe>
2024-06-17 09:55:57 -0500

    back-end

---
+++ Snacker/src/main/java/pe/edu/vallegrande/Snacker/SnackerApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.Snacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackerApplication.class, args);
+	}
+
+}
---
+++ Snacker/src/test/java/pe/edu/vallegrande/Snacker/SnackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.Snacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
